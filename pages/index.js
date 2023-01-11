6e3b0e12a5cfbf5727e32a85a0839238ea31c753
btdrick <b.drick@outlook.com>
2023-01-11 18:05:00 -0500

    Designed initial Product layout

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,8 @@ const Home = ({ products, bannerData }) => (
     </div>
 
     <div className='products-container'>
-      {['Product 1', 'Product 2'].map(
-        (product) => product)}
+      {products?.map((product) => <Product 
+      key={product._id} product={product} />)}
     </div>
 
     <FooterBanner />
