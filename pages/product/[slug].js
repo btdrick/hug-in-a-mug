89e8196f8cf3fdfbd5fb1ea15ccf079131c54ecb
btdrick <b.drick@outlook.com>
2023-01-13 17:33:06 -0500

    Added event handler for product "buy now" button

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,7 +7,14 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
     const [index, setIndex] = useState(0);
-    const { decQty, incQty, qty, onAdd } = useStateContext();
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+    //Event handler for click Buy Now button
+    const handleBuyNow = () => {
+        onAdd(product, qty);
+        setShowCart(true);
+    }
+
     return (
         <div>
             <div className='product-detail-container'>
@@ -58,7 +65,7 @@ const ProductDetails = ({ product, products }) => {
                         onClick={() => onAdd(product, qty)}>Add to Cart</button>
                         <button type='button'
                         className='buy-now'
-                        onClick=''>Buy Now</button>
+                        onClick={handleBuyNow}>Buy Now</button>
                     </div>
                 </div>
             </div>
